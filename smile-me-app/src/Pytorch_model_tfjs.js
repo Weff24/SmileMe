1e24320bd57832ca02b7c854165bed5c96865e49
ding810 <andydong2024@u.northwestern.edu>
2023-03-06 21:14:30 -0600

    working version of model

--- smile-me-app/src/Pytorch_model_tfjs.js
+++ smile-me-app/src/Pytorch_model_tfjs.js
@@ -1,33 +1,33 @@
 import * as tf from '@tensorflow/tfjs';
 import { loadGraphModel } from "@tensorflow/tfjs-converter";
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
-import Webcam from "react-webcam";
-import './Pytorch_model_onnx.css'
+// import Webcam from "react-webcam";
+import './Pytorch_model_tfjs.css'
 import { canvasToTensor } from './utils';
 
 const Pytorch_model_tfjs = () => {
     tf.setBackend('webgl');
     const MODEL_URL = 'tf_tfjs_model/model.json';
     const image_size = 48;
-    const webcamRef = useRef(null);
-    const videoConstraints = {
-        facingMode: "user",
-        mirrored: true
-    };
+    const videoRef = useRef(null);
+    // const videoConstraints = {
+    //     facingMode: "user",
+    //     mirrored: true
+    // };
     const moods = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"};
     
-    const initialImg = new Image();
-    initialImg.width = image_size;
-    initialImg.height = image_size;
-    initialImg.style = {display: "none"};
-    const [imgSrc, setImgSrc] = useState('');
-    const canvas = useRef();
+    // const initialImg = new Image();
+    // initialImg.width = image_size;
+    // initialImg.height = image_size;
+    // initialImg.style = {display: "none"};
+    // const [imgSrc, setImgSrc] = useState('');
+    const canvasRef = useRef();
     const [moodPrediction, setMoodPrediction] = useState("");
 
     let model;
     const loadModel = async () => {
         model = await loadGraphModel('tf_saved_model_tfjs/model.json');
-        console.log(model)
+        console.log(model);
     };
 
     function argMax(array) {
@@ -43,25 +43,48 @@ const Pytorch_model_tfjs = () => {
       }
 
     const runModel = async () => {
-        const newImg = new Image();
-        const newImgSrc = webcamRef.current.getScreenshot();
-        setImgSrc(newImgSrc);
-        newImg.src = newImgSrc;
-        newImg.width = image_size;
-        newImg.height = image_size;
-        const ctx = canvas.current.getContext("2d");
-		canvas.current.width = image_size;
-		canvas.current.height = image_size;
+        // const newImg = new Image();
+        // const newImgSrc = webcamRef.current.getScreenshot();
+        // setImgSrc(newImgSrc);
+        // newImg.src = newImgSrc;
+        // newImg.width = image_size;
+        // newImg.height = image_size;
+        // document.getElementById('srcImg').src = newImgSrc;
+        const ctx = canvasRef.current.getContext("2d");
+		canvasRef.current.width = image_size;
+		canvasRef.current.height = image_size;
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        ctx.drawImage(newImg, 0, 0, image_size, image_size);
-        const onnxTensor = await canvasToTensor("srcCanvas");
-        const prediction = await model.execute(onnxTensor);
+        ctx.drawImage(videoRef.current, 0, 0, image_size, image_size);
+        const prediction_tensor = await canvasToTensor("srcCanvas");
+        console.log("HERE")
+        console.log("Prediction tensor: " + prediction_tensor);
+        const prediction = await model.execute(prediction_tensor);
         let prediction_data = await prediction.data();
         let prediction_mood = moods[argMax(prediction_data)]
         console.log("HERE")
-        console.log(prediction_data)
+        console.log("Prediction: " + prediction)
+        console.log("Prediction data:" + prediction_data)
+        console.log("argMax: " + argMax(prediction_data))
+        console.log("mood: " + moods[argMax(prediction_data)])
         setMoodPrediction(prediction_mood);
+        setTimeout(runModel, 100);
     }
+
+    useLayoutEffect(() => {
+		navigator.mediaDevices
+			.getUserMedia({
+				audio: false,
+				video: {
+					facingMode: "user"
+				}
+			})
+			.then(stream => {
+				videoRef.current.srcObject = stream;
+				videoRef.current.onloadedmetadata = () => {
+					videoRef.current.play();
+				};
+			});
+	}, [videoRef]);
 
     useLayoutEffect(() => {
 		const init = async () => {
@@ -69,18 +92,19 @@ const Pytorch_model_tfjs = () => {
 			await runModel();
 		};
 		init();
-	});
+	}, []);
 
     return <div className='model_container'>
-        <Webcam 
+        {/* <Webcam 
             audio={false}
             ref={webcamRef}
             screenshotFormat="image/jpeg"
             videoConstraints={videoConstraints}
             mirrored={true}
-        />;
-        <img src={imgSrc} width={image_size} height={image_size} style={{display:"none"}}/>
-        <canvas id='srcCanvas' ref={canvas} width={image_size} height={image_size} style={ {display: "none"} }/>
+        />; */}
+        {/* <img id='srcImg' src={imgSrc} width={image_size} height={image_size} style={{display:"none"}}/> */}
+        <video autoPlay playsInline ref={videoRef}/>
+        <canvas id='srcCanvas' ref={canvasRef} width={image_size} height={image_size} style={{display:"none"}}/>
         <h3 className='moodPrediction'>Mood: {moodPrediction}</h3>
     </div>
 }
